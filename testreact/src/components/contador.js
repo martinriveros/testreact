682a030ef02d9009e95cbfa5039f2a71623dd5c6
Martin Riveros <martin.riveros@hotmail.com>
2021-07-13 18:28:28 -0300

    botones con disabled

--- testreact/src/components/contador.js
+++ testreact/src/components/contador.js
@@ -8,19 +8,19 @@ export const ItemCount = () => {
   let initCart=0
   let [stock, setStock] = useState(data[1].stock); // "data" tiene los productos.
   let [cart, setCart] = useState(initCart); // tiene el contenido del cart en el inicio.
-  let [visibilityAdd, setVisibilityAdd] = useState({visibility:'visible'}) //guarda el estado del boton de agregar
-  let [visibilitySub, setVisibilitySub] = useState({visibility:'hidden'}) //guarda el estado del boton de quitar
+  let [visibilityAdd, setVisibilityAdd] = useState(false) //guarda el estado del boton de agregar
+  let [visibilitySub, setVisibilitySub] = useState(true) //guarda el estado del boton de quitar
   
   const addOne = () =>{
       if(stock>0){
         
         setCart(cart+=1);
         setStock(stock-=1);
-        setVisibilitySub({visibility:'visible'})
+        setVisibilitySub(false)
         data[1].stock-=1
           
         if(data[1].stock===0)
-          {setVisibilityAdd({visibility:'hidden'})}
+          {setVisibilityAdd(true)}
         
         }}  
 
@@ -29,11 +29,11 @@ export const ItemCount = () => {
       if(cart>0){
         setCart(cart-=1);
         setStock(stock+=1);
-        setVisibilityAdd({visibility:'visible'})
+        setVisibilityAdd(false)
         data[1].stock+=1       
         
         if(cart===0)
-          {setVisibilitySub({visibility:'hidden'})}
+          {setVisibilitySub(true)}
         }}
   
   return (
@@ -43,8 +43,8 @@ export const ItemCount = () => {
         <h1>{stock}</h1>
         <h2>Cantidad pedida</h2>
         <h1>{cart}</h1>
-        <button className='btn' style={visibilityAdd} onClick={()=>addOne()}>agregar</button>
-        <button className='btn' style={visibilitySub} onClick={()=>substractOne()}>quitar</button>
+        <button className='btn' disabled={visibilityAdd} onClick={()=>addOne()}>agregar</button>
+        <button className='btn' disabled={visibilitySub} onClick={()=>substractOne()}>quitar</button>
         <button className='btn' onClick={()=> {return 1}}>Agregar al Carrito</button>
 
 
--- testreact/src/index.js
+++ testreact/src/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React from 'react'; // no se necesita ruta porque importa dependencia desde node_modules
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.css'; // no importa nada que se haya exportado
 import App from './App';
 
 ReactDOM.render(
